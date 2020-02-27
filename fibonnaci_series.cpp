e69a92b9b98c20bedc900bc56c1805f390bb195d
Sandeep <vsandeepsekhar@gmail.com>
2020-02-26 19:29:49 -0500

    fibonnachi series

--- fibonnaci_series.cpp
+++ fibonnaci_series.cpp
@@ -2,22 +2,20 @@
 
 using namespace std;
 
-void fibonnaci_series(){
+void fibonnaci_series () {
     static int f_series_1 = 0;
     static int f_series_2 = 1;
     static int sum = 0;
-    int fb_limit;
-    cout<<"Enter the fb_limit:\t"<<endl;
-    cin<<fb_limit;
-    //scanf("%d",&fb_limit);
+    int fb_limit, random;
+    cout << "Enter the fb_limit:\t"<<endl;
+    cin >> fb_limit;
 
     while (sum < fb_limit){
-      //printf("Value of sum: %d",f_series_1);
-      cout<<"Value of sum: %d"<<f_series_1;
+      cout<<"Value of sum: "<<f_series_1;
       sum = f_series_1 + f_series_2;
       f_series_1 = f_series_2;
       f_series_2 = sum;
-      getch();
+      std::getchar();
     }
 }
 
